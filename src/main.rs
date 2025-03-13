9ada57fb7fcbe2591475dd003bda8915f162a0e4
trudransh <trudranshsingh2003@gmail.com>
2025-03-13 16:26:37 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     io::stdout().flush().unwrap();
 
     // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    let stdin = io::stdin(); // read input from user    
+    let mut input = String::new(); // create a new string to store the input
+    stdin.read_line(&mut input).unwrap(); // read the input from the user
+    println!("{}: command not found", input.trim()) // print the input from the user
 }
