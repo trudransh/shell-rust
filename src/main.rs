46c8baeee9e9a70a491cfad5a640398a1c9b5f98
trudransh <trudranshsingh2003@gmail.com>
2025-03-13 22:09:08 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    let mut input = String::new();
+    
     loop {
         print!("$ ");
-        io::stdout().flush().unwrap();
-
-        // Wait for user input
+        io::stdout().flush().unwrap();// Wait for user input
         let stdin = io::stdin(); // read input from user
         // create a new string to store the input
+        let mut input = String::new();
         stdin.read_line(&mut input).unwrap(); // read the input from the user and store it in input (unwrap() is used to handle the error if the input is not read)
         
         match input.trim() {
