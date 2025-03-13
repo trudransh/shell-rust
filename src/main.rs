50e7d631986448a94a46854bce5285262422fa4e
trudransh <trudranshsingh2003@gmail.com>
2025-03-13 16:46:59 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
+    let mut input = String::new();
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         // Wait for user input
         let stdin = io::stdin(); // read input from user
-        let mut input = String::new(); // create a new string to store the input
+        // create a new string to store the input
         stdin.read_line(&mut input).unwrap(); // read the input from the user
-        println!("{}: command not found", input.trim()); // print the input from the user
+        if input.ends_with('\n') {
+            input.pop();
+        }
+        println!("{}: command not found", input.trim()); 
+        input.clear();// print the input from the user
         break;
     }
 }
