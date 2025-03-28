049cabe840362d213430fb2210f7c624650ec036
trudransh <trudranshsingh2003@gmail.com>
2025-03-28 16:43:50 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 use std::collections::HashMap;
 use std::env;
+use std::path::Path;
 use lazy_static::lazy_static;
 
 // lazy_static creates a global variable that is:
@@ -55,25 +56,28 @@ fn main() {
                     println!("{} is a shell builtin", tail);
                 } else {
                         //  This is where you need to add PATH searching logic
-                        // TODO 1. Get PATH environment variable
-                        
-                        // TODO 2. Split PATH into directories
-                        // TODO 3. Check each directory for an executable with name "tail"
-                        // TODO 4. If found, print the full path
-                        // TODO 5. If not found anywhere, then print "not found"
-
                         match env::var("PATH"){
                             Ok(path) => {
-                                println!("{}", path);
-                                let directories = path.split(":");
-                                
+                                let mut found = false;
+                                for dir in path.split(":"){
+                                    let file_path = Path::new(dir).join(&tail);
+                                    if file_path.exists(){
+                                        println!("{} is {}", tail,file_path.display());
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if !found{
+                                    println!("{}: not found", tail);
+                                }
+
                             },
                             Err(e)=> {
-                                println!("{}", e);
+                                println!("{}: not found", tail);
                             }
                         }               
 
-                    println!("{}: not found", tail);
+                    
                 }
             },
             
